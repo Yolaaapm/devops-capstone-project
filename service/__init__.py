ddd9a627a9724329389baa13198f3736f2b095c2
Yolaaa <monikfiola@gmail.com>
2026-05-16 00:24:41 +0700

    Clean up comments in __init__.py
    Removed comment about avoiding circular imports.

--- service/__init__.py
+++ service/__init__.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 
 # Inisialisasi Security
 CORS(app)
+# Pastikan tidak ada teks 'lokal/lab' tanpa tanda pagar di sini
 talisman = Talisman(app, force_https=False)
 
-# Import routes dan models di bawah agar tidak circular import
-# Kita tambahkan noqa agar Flake8 tidak protes
 from service import routes, models  # noqa: F401, E402
