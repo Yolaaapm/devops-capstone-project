a78ab8628c2423a2386f45024a8df39240999c4f
Yolaaa <monikfiola@gmail.com>
2026-05-16 00:10:27 +0700

    Add CORS configuration to Flask app
    Added CORS configuration for testing environment.

--- service/__init__.py
+++ service/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
+
 app = Flask(__name__)
-# Konfigurasi CORS (Cross-Origin Resource Sharing)
+
+# Tambahkan baris ini agar aplikasi tidak error saat testing
 CORS(app)
-# Konfigurasi Talisman untuk Security Headers
-# Memaksa HTTPS didefinisikan sebagai False jika di lingkungan
-lokal/lab
 talisman = Talisman(app, force_https=False)
+
 from service import routes, models
